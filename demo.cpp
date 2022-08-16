12a2103645e0d5a3e1ee7f74302b31da7fbc6b34
uug4na <65070195+uug4na@users.noreply.github.com>
2022-08-16 23:18:44 +0800

    test

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,10 @@
 #include <Windows.h>
 #include <WinUser.h>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
 #define FILE_MENU_NEW 1
+using namespace std;
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 void Menus(HWND hWnd);
@@ -51,7 +55,7 @@ int WINAPI main(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
     if (!RegisterClassW(&wc))
         return -1;
 
-    CreateWindowW(L"workPlease", L"plsplspls", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500,
+    CreateWindowW(L"workPlease", L"Windowontsor", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500,
         NULL, NULL, NULL, NULL);
 
     MSG msg = { 0 };
@@ -85,6 +89,32 @@ void Menus(HWND hWnd)
 
 void AddText(HWND hWnd)
 {
-    CreateWindowW(L"workPlease", L"lmao", WS_CHILD | WS_VISIBLE | WS_BORDER, 200, 100, 100, 50,
+    CreateWindowW(L"workPlease", L"lma0", WS_CHILD | WS_VISIBLE | WS_BORDER, 200, 100, 100, 50,
         hWnd, NULL, NULL, NULL);
 }
+
+void encrypt() {
+    char fileName[30], ch;
+    fstream fin, fout;
+    cout << "Enter File NAme:)";
+    cin >> fileName;
+    fin.open(fileName, fstream::in);
+    if (!fin)
+        cout << "errorrRR:RL:)";
+    fout.open("enc.txt", fstream::out);
+    while (fin >> noskipws >> ch) {
+        ch += 100;
+        fout << ch;
+    }
+    fin.close();
+    fout.close();
+    fin.open(fileName, fstream::out);
+    fout.open("enc.txt", fstream::in);
+    while (fout >> noskipws >> ch) {
+        fin << ch;
+    }
+    fin.close();
+    fout.close();
+    cout << "donekk:" << fileName << endl;
+}
+
