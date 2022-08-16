3afe549b913ff8e24e51a2da1cae1b1ed2203692
uug4na <65070195+uug4na@users.noreply.github.com>
2022-08-17 01:38:17 +0800

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -2,15 +2,40 @@
 #include <WinUser.h>
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
 #define FILE_MENU_NEW 1
 using namespace std;
+
+void encrypt() {
+    char fileName[30], ch;
+    fstream fin, fout;
+    cout << "Enter File NAme:)";
+    cin >> fileName;
+    fin.open(fileName, fstream::in);
+    if (!fin)
+        cout << "errorrRR:RL:)";
+    fout.open("enc.txt", fstream::out);
+    while (fin >> noskipws >> ch) {
+        ch += 100;
+        fout << ch;
+    }
+    fin.close();
+    fout.close();
+    fin.open(fileName, fstream::out);
+    fout.open("enc.txt", fstream::in);
+    while (fout >> noskipws >> ch) {
+        fin << ch;
+    }
+    fin.close();
+    fout.close();
+    cout << "donekk:" << fileName << endl;
+}
+
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 void Menus(HWND hWnd);
 void AddText(HWND);
 
-HMENU hMenu; // global scope
+HMENU hMenu;
 
 LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
@@ -21,6 +46,7 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
         {
         case 1:
             MessageBeep(MB_OK);
+            encrypt();
             break;
         case 2:
             MessageBeep(MB_HELP);
@@ -64,11 +90,9 @@ int WINAPI main(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
     return 0;
 }
 
-// menus
 void Menus(HWND hWnd)
 {
     hMenu = CreateMenu();
@@ -79,7 +103,7 @@ void Menus(HWND hWnd)
     AppendMenu(hSubMenu, MF_STRING, NULL, L"Connect");
     AppendMenu(hSubMenu, MF_STRING, NULL, L"Host");
 
-    AppendMenu(hFileMenu, MF_STRING, 1, L"Name");
+    AppendMenu(hFileMenu, MF_STRING, 1, L"Encrypt");
     AppendMenu(hFileMenu, MF_POPUP, (UINT_PTR)hSubMenu, L"Room");
     AppendMenu(hFileMenu, MF_STRING, 4, L"Exit");
     AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, L"Menu");
@@ -89,34 +113,14 @@ void Menus(HWND hWnd)
 
 void AddText(HWND hWnd)
 {
-    CreateWindowW(L"workPlease", L"lma0", WS_CHILD | WS_VISIBLE | WS_BORDER, 200, 100, 100, 50,
-        hWnd, NULL, NULL, NULL);
+    CreateWindow(
+        L"BUTTON", L"Encrypt", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 10,40,130,100,hWnd,NULL,       
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),NULL);    
+    CreateWindow(
+        L"BUTTON", L"Decrypt", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 150,40, 130, 100, hWnd, NULL,
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
 }
 
-void encrypt() {
-    char fileName[30], ch;
-    fstream fin, fout;
-    cout << "Enter File NAme:)";
-    cin >> fileName;
-    fin.open(fileName, fstream::in);
-    if (!fin)
-        cout << "errorrRR:RL:)";
-    fout.open("enc.txt", fstream::out);
-    while (fin >> noskipws >> ch) {
-        ch += 100;
-        fout << ch;
-    }
-    fin.close();
-    fout.close();
-    fin.open(fileName, fstream::out);
-    fout.open("enc.txt", fstream::in);
-    while (fout >> noskipws >> ch) {
-        fin << ch;
-    }
-    fin.close();
-    fout.close();
-    cout << "donekk:" << fileName << endl;
-}
 
 ///void decrypt() {
 ///    cout << "decrypting kk:)";
